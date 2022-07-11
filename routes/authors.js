a21e4b2ded7e115ccad8c672f99e0627fffb73f5
Jonathan Foo <jonathan.foozh@gmail.com>
2022-07-11 21:46:07 +1000

    Created Book Index/New/Create Routes

---
+++ routes/authors.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const router = express.Router();
+const Author = require('../models/author');
+
+
+router.get('/', async (req, res) => {
+    const searchOptions = {};
+    if (req.query.name != null && req.query.name !== '') {
+        searchOptions.name = new RegExp(req.query.name, 'i')
+    }
+    try {
+        const authors = await Author.find(searchOptions);
+        res.render('authors/index', { 
+            authors: authors, 
+            searchOptions: req.query
+        }); 
+    } catch(err) {
+        res.redirect('/')
+    }
+})
+
+router.get('/new', (req, res) => {
+    res.render('authors/new', {author: new Author()});
+})
+
+router.post('/', async(req, res) => {
+    const author = new Author({
+        name: req.body.name
+    })
+    try {
+        const newAuthor = await author.save();
+        // res.render(`authors/${author.id}`)
+        res.redirect('authors');
+    } catch(errr) {
+        res.render('authors/new', {
+            author: author,
+            errorMessage: 'Error Creating Author'
+        });
+    }
+})
+
+module.exports = router;
--- server.js
+++ server.js
@@ -6,17 +6,22 @@ const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
 const mongoose = require('mongoose');
 const indexRouter = require('./routes/index');
+const authorRouter = require('./routes/authors');
+const bookRouter = require('./routes/books');
 const app = express();
 
 app.set('view engine', 'ejs');
 app.set('layout', 'layouts/layout');
 app.use(expressLayouts);
 app.use(express.static('public'));
+app.use(express.urlencoded({extended: false, limit: '10mb'}));
 
 mongoose.connect(process.env.DATABASE_URL);
-const db = mongoose.connection;
-db.on('error', err => console.error(err));
-db.on('open', () => console.log('Connected To Server'));
+// const db = mongoose.connection;
+// db.on('error', err => console.error(err));
+// db.on('open', () => console.log('Connected To Server'));
 
 app.use('/', indexRouter);
+app.use('/authors', authorRouter);
+app.use('/books', bookRouter);
 app.listen(process.env.PORT || 3000);
